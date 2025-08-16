543e4730f8e1ea7ff98c0f52a010193ae24aec0f
usarabiul <usarabiul@gmail.com>
2025-08-16 23:56:29 +0600

    Report page UI Update

--- resources/views/admin/reports/postReports.blade.php
+++ resources/views/admin/reports/postReports.blade.php
@@ -15,30 +15,8 @@
 </style>
 @endpush @section('contents')
 
-<div class="content-header row">
-    <div class="content-header-left col-md-6 col-12 mb-2">
-        <h3 class="content-header-title mb-0">Posts Reports</h3>
-        <div class="row breadcrumbs-top">
-            <div class="breadcrumb-wrapper col-12">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}">Dashboard </a></li>
-                    <li class="breadcrumb-item active">Posts Report</li>
-                </ol>
-            </div>
-        </div>
-    </div>
-    <div class="content-header-right col-md-6 col-12 mb-md-0 mb-2">
-        <div class="btn-group float-md-right" role="group" aria-label="Button group with nested dropdown">
-            <a class="btn btn-outline-primary" href="{{route('admin.reportsAll','posts')}}"> <i class="fa-solid fa-rotate"></i> </a>
-        </div>
-    </div>
-</div>
 
-<div class="content-body">
-    <!-- Basic Elements start -->
-    <section class="basic-elements">
-        <div class="row">
-            <div class="col-md-12">
+
                 @include(adminTheme().'alerts')
 
                 <div class="card">
@@ -154,10 +132,6 @@
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </section>
-</div>
 
 @endsection 
 
